37ef248f451899d72ed5de62a2f2f0a202c30ded
Maria Grivorieva <masha_21.02.99@mail.ru>
2021-08-19 12:59:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static AutoResetEvent waitHandler = new AutoResetEvent(true);
+        static int x = 0;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+        public static void Count()
+        {
+            waitHandler.WaitOne();
+            x = 1;
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                x++;
+                Thread.Sleep(100);
+            }
+            waitHandler.Set();
+        }
+    }
+}
